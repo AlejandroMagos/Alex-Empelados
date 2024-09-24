58dfed5bae2fc9c94f8b6755e8c542edd441a4a6
Maguitos <alex_magos@hotmail.com>
2024-09-24 10:42:27 -0600

    First Commit

---
+++ WebApiPerson/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApiPerson.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public required string A_P { get; set; }
+        public required string A_M { get; set; }
+        public required int Edad { get; set; }
+        public required char Sexo { get; set; }
+    }
+}
